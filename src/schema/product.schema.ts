d63e40c30acfa04b872f0e7d5f35d30ec4896687
Sahid Rahman <sahidrahman404@gmail.com>
2022-07-29 18:53:58 +0700

    add schema validation with zod to validate users input

---
+++ src/schema/product.schema.ts
@@ -0,0 +1,52 @@
+import { object, number, string, TypeOf } from "zod";
+
+const payload = {
+  body: object({
+    title: string({
+      required_error: "Title is required",
+    }),
+    description: string({
+      required_error: "Description is required",
+    }).min(120, "Description should be at least 120 characters long"),
+    price: number({
+      required_error: "Price is required",
+    }),
+    image: string({
+      required_error: "Image is required",
+    }),
+  }),
+};
+
+const params = {
+  params: object({
+    id: string({
+      required_error: "ID is required",
+    }),
+  }),
+};
+
+export const createProductSchema = object({
+  ...payload,
+});
+
+export const updateProductSchema = object({
+  ...payload,
+  ...params,
+});
+
+export const getProductSchema = object({
+  body: object({
+    title: string({
+      required_error: "Title is required",
+    }),
+  }),
+});
+
+export const deleteProductSchema = object({
+  ...params,
+});
+
+export type CreateProductInput = TypeOf<typeof createProductSchema>;
+export type UpdateProductInput = TypeOf<typeof updateProductSchema>;
+export type ReadProductInput = TypeOf<typeof getProductSchema>;
+export type DeleteProductInput = TypeOf<typeof deleteProductSchema>;
