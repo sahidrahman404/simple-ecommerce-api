a8db1bc9fa6f224533058428ad0b24640fb5964a
Sahid Rahman <sahidrahman404@gmail.com>
2022-07-29 18:55:02 +0700

    add middleware to the express server

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import deSerializeUser from "./middleware/deserializeUser";
+import routes from "./routes";
 
 export default () => {
   const app = express();
 
+  app.use(deSerializeUser);
   app.use(express.json());
+  app.use(routes);
 
   return app;
 };
